1b016be6025e26615603735bee7019dabf47b3b5
krapnik <krapnik@qq.com>
2021-07-20 12:02:28 +0800

    更新polea的配置

--- generators/app/templates/.polea/config.ts
+++ generators/app/templates/.polea/config.ts
@@ -3,15 +3,40 @@ import { getversion } from "./getVersion";
 let config: polea.ConfigManager = {
     buildConfig: (params: polea.ConfigCommand) => {
         let { command } = params;
-        if (command == "compile") {
+        if (command == "compile") {//polea compile
             return {
+                define: { DEBUG: true, RELEASE: false },
+                watch: false,
                 output: "./bin",
-                plugins: [new polea.ESBundlePlugin({ define: { DEBUG: true, RELEASE: false }, sourcemap: true })],
+                plugins: [new polea.ESBundlePlugin({ sourcemap: true })],
             };
-        } else if (command == "publish") {
+        } else if (command == "publish") {//polea publish
             return {
+                define: { DEBUG: true, RELEASE: false },
+                watch: false,
                 output: "./release/" + getversion(),
-                plugins: [new polea.ESBundlePlugin({ define: { DEBUG: true, RELEASE: false }, sourcemap: false, minify: true })],
+                plugins: [new polea.ESBundlePlugin({ sourcemap: false, minify: true })],
+            };
+        } else if (command == "watch") {//polea watch
+            return {
+                define: { DEBUG: true, RELEASE: false },
+                watch: true,
+                output: "./bin",
+                plugins: [new polea.ESBundlePlugin({ sourcemap: true })],
+            };
+        } else if( command == "watch-ui"){
+            return {
+                define: { DEBUG: true, RELEASE: false },
+                watch: true,
+                output: "./bin",
+                plugins: [new polea.UIPlugin()],
+            };
+        } else if( command == "watch-all"){
+            return {
+                define: { DEBUG: true, RELEASE: false },
+                watch: true,
+                output: "./bin",
+                plugins: [new polea.ESBundlePlugin({ sourcemap: true }),new polea.UIPlugin()],
             };
         }
     },
